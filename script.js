8651804fd2381a77966cdf6e81696f110b2fa987
Florian Sellier <contact@floriansellier.com>
2019-02-01 08:44:00 +0100

    Module changement de bg par GET

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ var joueur1, joueur2;
 window.addEventListener("load",initialisation, false);
 function initialisation() {
     joueur1 = document.getElementById("joueur1");
-    joueur1.addEventListener("click",test,false);
     joueur2 = document.getElementById("joueur2");
-    joueur2.addEventListener("click",test,false);
-}
-
-function changeBGImage(Image){
-    if (document.body){
-    document.body.style.backgroundImage = "url('IMG/"+Image+"')";
-    }
+    joueur2.addEventListener("click",test,false)
 }
 
 var tempsretourne1;
