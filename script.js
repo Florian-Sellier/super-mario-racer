3819b8cd4fa03f0bae2dee43b14bf212591b313d
Jules Paumier <jules.paumier76@gmail.com>
2019-02-01 08:39:10 +0100

    Fonction Georgie finie + retour au départ

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ var joueur1, joueur2;
 window.addEventListener("load",initialisation, false);
 function initialisation() {
     joueur1 = document.getElementById("joueur1");
-    joueur1.addEventListener("click",test,false);
     joueur2 = document.getElementById("joueur2");
-    joueur2.addEventListener("click",test,false);
 }
 
 function changeBGImage(Image){
@@ -14,37 +12,56 @@ function changeBGImage(Image){
 }
 
 var tempsretourne1;
-var tempsAle1 = Math.floor(Math.random() * 5000) + 1000;
+var tempsAle1 = Math.floor(Math.random() * 5000) + 2000;
 
-
+var georgieRet;
 var tempsretourne2;
 var tempsAle2 = Math.floor(Math.random() * 2000) + 1000;
 
+var tempsprep;
 
 var divGeorgie = document.getElementById( 'georgie' );
 divGeorgie.style.backgroundColor = 'green';
 
 
 document.ready(georgie1());
+document.ready(initialisation());
 
 function georgie1() {
   tempsretourne1 = setTimeout(retourne1, tempsAle1);
 }
 
 function georgie2() {
-  tempsretourne2 = setTimeout(retourne2, tempsAle2);
+  tempsretourne2 = setTimeout(retourne2, 500);
+}
+
+function georgiePrep() {
+  tempsprep = setTimeout(prep, tempsAle2);
 }
 
 function retourne1() {
-	var tempsAle1 = Math.floor(Math.random() * 5000) + 1000;
+	var tempsAle1 = Math.floor(Math.random() * 5000) + 2000;
+	var tempsAle2 = Math.floor(Math.random() * 2000) + 1000;
 	console.log (tempsAle1);
+	console.log(georgieRet);
+	georgieRet=false;
 	divGeorgie.style.backgroundColor = 'green';
-	georgie2();
+	georgiePrep();
 }
 
 function retourne2() {
-	var tempsAle2 = Math.floor(Math.random() * 5000) + 1000;
+	var tempsAle1 = Math.floor(Math.random() * 5000) + 2000;
+	var tempsAle2 = Math.floor(Math.random() * 2000) + 1000;
 	console.log (tempsAle1);
+	console.log(georgieRet);
+	georgieRet=true;
 	divGeorgie.style.backgroundColor = 'red';
 	georgie1();
+}
+
+function prep() {
+	var tempsAle1 = Math.floor(Math.random() * 5000) + 2000;
+	var tempsAle2 = Math.floor(Math.random() * 2000) + 1000;
+	divGeorgie.style.backgroundColor = 'orange';
+	georgie2();
 }
